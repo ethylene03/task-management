9571cf167b93fa279233aa3dd60940fb35a4a2a1
Princess Ethel Atillo <peatillo@vauldex.com>
2025-12-02 08:12:14 +0800

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import HomePage from '@/views/HomePage.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', name: 'Home', component: HomePage }
+  ],
+})
+
+export default router
